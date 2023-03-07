c4650561b180d00ad2d28efa32892cf2afee7310
gmtavares <gmtavares2.0@gmail.com>
2023-03-07 18:31:43 +0000

    Added verification checks for the FIFO's existence

--- my_fifo.c
+++ my_fifo.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct {
 	int *buffer;
@@ -9,16 +10,30 @@ typedef struct {
 } FIFO;
 
 void MyFIFOInit(FIFO *fifo, int size) {
+	if (!fifo) {
+		printf("FIFO pointer is NULL\n");
+		return;
+	}
 	fifo->buffer = (int*)malloc(size * sizeof(int));
+	if (!fifo->buffer) {
+		printf("Failed to allocate buffer\n");
+		return;
+	}
 	fifo->size = size;
 	fifo->head = 0;
 	fifo->tail = -1;
 	fifo->count = 0;
+	printf("FIFO initialized with size %d\n", size);
 }
 
 void MyFIFOInsert(FIFO *fifo, int item) {
+	if (!fifo) {
+		printf("FIFO pointer is NULL\n");
+		return;
+	}
 	if (fifo->count == fifo->size) {
-		return; // FIFO is full
+		printf("FIFO is full\n");
+		return;
 	}
 	fifo->tail = (fifo->tail + 1) % fifo->size;
 	fifo->buffer[fifo->tail] = item;
@@ -26,8 +41,13 @@ void MyFIFOInsert(FIFO *fifo, int item) {
 }
 
 int MyFIFORemove(FIFO *fifo) {
+	if (!fifo) {
+		printf("FIFO pointer is NULL\n");
+		return 0;
+	}
 	if (fifo->count == 0) {
-		return 0; // FIFO is empty
+		printf("FIFO is empty\n");
+		return 0;
 	}
 	int item = fifo->buffer[fifo->head];
 	fifo->head = (fifo->head + 1) % fifo->size;
@@ -36,12 +56,21 @@ int MyFIFORemove(FIFO *fifo) {
 }
 
 int MyFIFOPeep(FIFO *fifo) {
+	if (!fifo) {
+		printf("FIFO pointer is NULL\n");
+		return 0;
+	}
 	if (fifo->count == 0) {
-		return 0; // FIFO is empty
+		printf("FIFO is empty\n");
+		return 0;
 	}
 	return fifo->buffer[fifo->head];
 }
 
 int MyFIFOSize(FIFO *fifo) {
+	if (!fifo) {
+		printf("FIFO pointer is NULL\n");
+		return 0;
+	}
 	return fifo->count;
 }
